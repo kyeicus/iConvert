ee4b5ee2bc85f0e95969efcc8e4f1afd0b115229
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-04-09 07:27:04 +0000

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Emmanuel K. Nketia on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
